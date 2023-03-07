19ce7c3d51fdb0e21e6d08fb12f29d846948f5cc
Bjorn Hansen <blmhansen@gmail.com>
2023-03-06 22:05:06 -0800

    Start to add item editing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {React, useState} from 'react';
 import Header from "./components/Header";
 import List from "./components/List";
 import AddItemField from "./components/AddItemField";
--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 
 export default function ListItem({listItem, handleDeleteItem, handleCheckItem}) {
+    const [editing, setEditing] = useState(false);
+
     return (
         <li className="flex border rounded space-x-2 p-2 mb-2">
             <input id={'checkbox' + listItem.id} type="checkbox" onChange={e => handleCheckItem(e, listItem.id)} checked={listItem.checked}/>
-            <div className={'grow' + (listItem.checked ? ' line-through' : '')}>{listItem.text}</div>
+            {editing ?
+                <input className="grow border rounded grow p-1" /> :
+                <div className={'grow' + (listItem.checked ? ' line-through' : '')}>{listItem.text}</div>
+            }
+            {editing ?
+                <button className="text-green-600" onClick={() => { setEditing(false) }}>Save</button> :
+                <button className="text-blue-600" onClick={() => { setEditing(true) }}>Edit</button>
+            }
             <button className="text-red-600" onClick={() => { handleDeleteItem(listItem.id) }}>Delete</button>
         </li>
     );
